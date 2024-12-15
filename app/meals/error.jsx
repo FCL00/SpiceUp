00f950d8de1226ac7da5ff698f3d1584553be510
FCL00 <flagahit00@gmail.com>
2024-12-15 13:59:51 +0800

    add meals error page

---
+++ app/meals/error.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Error(){
+    return <main className="error">
+        <h1>We have encountered an error</h1>
+        <p>
+           Lorem ipsum dolor sit, amet 
+           consectetur adipisicing elit. Reiciendis fugiat 
+           repudiandae earum, molestias magni dignissimos 
+           atque sed dolorum quas sint!
+        </p>
+    </main>
+}
