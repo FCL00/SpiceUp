a32172b944b2663dbcf410dab16dbbe19411a03d
FCL00 <flagahit00@gmail.com>
2024-12-15 14:01:18 +0800

    add header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavLink from "./partial/NavLink";
+
+// Image
+import LogoImg from "@/assets/logo.png";
+
+// Styles
+import styles from "@/components/Header/Header.module.css";
+
+
+export default function Header(){
+
+    return (
+       <header className={styles['header']}>
+            <Link className={styles['logo']} href="/">
+                <Image src={LogoImg} alt="A plate with food in it"  priority/>
+                Foodies
+            </Link>
+
+            <nav className={styles['nav']}>
+                <ul>
+                    <NavLink url="/meals" label="Browse Meals"/>
+                    <NavLink url="/community" label="Foodies Community"/>
+                </ul>
+            </nav>
+       </header>
+    );
+}
